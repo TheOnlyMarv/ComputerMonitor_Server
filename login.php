083bdb0e42f5224d0d341452ed4210a8e8134594
Marvin Vierling <marvinvierling@hotmail.de>
2016-04-29 16:37:33 +0200

    token will be the same for the next login

--- login.php
+++ login.php
@@ -5,18 +5,21 @@
 	require "DB.php";
 	
 	
-	$userid = DB::query( 'SELECT id FROM user WHERE username=? AND password=?', $username, $password );
+	$user = DB::query( 'SELECT * FROM user WHERE username=? AND password=?', $username, $password );
 	
-	if( empty( $userid ) ){
+	if( empty( $user ) ){
 		$returnvalue['status'] = false;
 		$returnvalue['message'] = "username or password incorrect";
 		echo json_encode($returnvalue);
 	}
 	else{
-		$token = md5(microtime());
-		DB::query( 'UPDATE user SET token=? WHERE id=?', $token, $userid[0]["id"] );
+		$token = $user[0]["token"];
+		if( empty($token) ){
+			$token = md5(microtime());
+			DB::query( 'UPDATE user SET token=? WHERE id=?', $token, $user[0]["id"] );
+			$token = DB::query( 'SELECT token FROM user WHERE token=?', $token)[0]["token"];
+		}
 		
-		$token = DB::query( 'SELECT token FROM user WHERE token=?', $token);
 		if( empty( $token ) ){
 			$returnvalue['status'] = false;
 			$returnvalue['message'] = "something went wrong";
@@ -24,7 +27,7 @@
 		else{
 			$returnvalue['status'] = true;
 			$returnvalue['message'] = "login successful";
-			$returnvalue['token'] = $token[0]['token'];
+			$returnvalue['token'] = $token;
 		}
 		echo json_encode( $returnvalue );
 	}
